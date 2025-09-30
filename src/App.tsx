232601cc70a4f26bdfcde3ffaf89d8546e567657
WylieYYYY <wylieyyyy@gmail.com>
2025-09-30 18:56:57 +0100

    Add SVG export and resize underlying SVG for better fit in export

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { GridGenerator, HexGrid, Layout } from 'react-hexgrid'
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 import { useShallow } from 'zustand/react/shallow'
@@ -5,6 +6,18 @@ import { GridPoint } from './GridPoint.tsx'
 import { CuboidStructureInputs } from './CuboidStructureInputs.tsx'
 import { IsometricStructure } from './IsometricStructure.tsx'
 import { useStore } from './Store.tsx'
+
+function downloadSVG(setDownloadUrl: (downloadUrl: string) => void) {
+  const BLOB_URL_TIMEOUT = 500
+
+  const svg = document.querySelector('svg')!
+  const anchor = document.getElementById('download') as HTMLAnchorElement
+
+  anchor.download = 'export.svg'
+  const blob = new Blob([svg.outerHTML], { type: 'image/svg+xml' })
+  setDownloadUrl(URL.createObjectURL(blob))
+  setTimeout(() => anchor.click(), BLOB_URL_TIMEOUT)
+}
 
 function App() {
   const [
@@ -23,31 +36,41 @@ function App() {
     state.rotateZAnticlockwise
   ]))
 
+  const [downloadUrl, setDownloadUrl] = useState('#')
+
   const generator = GridGenerator.hexagon(20)
 
   return (
     <>
+      <a id='download' href={downloadUrl} style={{ display: 'none' }}></a>
       <TransformWrapper centerOnInit={true} initialScale={8}>
         <TransformComponent wrapperStyle={{ width: '100%' }}>
-          <HexGrid style={{ width: 1000, height: 1000 }}>
-            <Layout size={{ x: 0.1, y: 0.1 }} spacing={4}>
-              {
-                generator.map((hex, key) => (
-                  <GridPoint
-                    key={key}
-                    hex={hex}
-                    spacing={4}
-                    radius={0.05}
-                  />
-                ))
-              }
-              <IsometricStructure />
-            </Layout>
-          </HexGrid>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 1000, height: 1000 }}>
+            <HexGrid viewBox='-20 -20 40 40'>
+              <Layout size={{ x: 0.1, y: 0.1 }} spacing={4}>
+                {
+                  generator.map((hex, key) => (
+                    <GridPoint
+                      key={key}
+                      hex={hex}
+                      spacing={4}
+                      radius={0.05}
+                    />
+                  ))
+                }
+                <IsometricStructure />
+              </Layout>
+            </HexGrid>
+          </div>
         </TransformComponent>
       </TransformWrapper>
       <div style={{ position: 'fixed', left: '.5em', top: '2em' }}>
         <CuboidStructureInputs />
+      </div>
+      <div style={{ position: 'fixed', left: '.5em', bottom: '2em' }}>
+        <button onClick={() => downloadSVG(setDownloadUrl)}>
+          Export SVG
+        </button>
       </div>
       <div style={{ position: 'fixed', right: '.5em', bottom: '2em', display: 'flex', flexDirection: 'column' }}>
         <button onClick={rotateXClockwise}>Rotate about positive x (→x)</button>
